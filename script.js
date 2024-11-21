4d8f3256acee824950e539ca8cc2e89ad066ab1b
MohanHS95 <mohanhsharan@gmail.com>
2024-11-21 16:08:29 -0500

    Add event listeners for functionality
    A mouseover event listener was added to manipulate each square node in
    the Nodelist, setting the color to green. The loop structure to create
    the grid was moved into a playGame function along with the event for
    the ability to call the function entirely when resetting the game with
    the added "Click to Reset" button.
    
    A prompt appears when the button is clicked and the input is used to
    create a new grid to play the game once again.

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const container = document.querySelector("#container");
 
+const resetButton = document.createElement("button");
+resetButton.setAttribute("id","reset");
+resetButton.setAttribute("style", "border: solid 1px; height: 40px; width: 80px");
+resetButton.textContent = "Click to Reset";
+container.appendChild(resetButton);
+
+function createGame(input) {
 const grid = document.createElement("div");
 grid.setAttribute("id", "grid");
 grid.setAttribute("style", "border: solid 0px; height: 600px; width: 600px");
 container.appendChild(grid);
 
 
-let input = 64;
 const size = (600) / (input);
-
 for (let i = 0; i < input; i++) {
         const squarerow = document.createElement('div');
         squarerow.classList.add('squarerow');
@@ -21,3 +26,29 @@ for (let i = 0; i < input; i++) {
     }
     grid.appendChild(squarerow);
 }
+
+const squares = document.querySelectorAll(".square");
+
+squares.forEach((square) => {
+        square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "green";
+    });
+});
+}
+let input = 64;
+createGame(input);
+
+resetButton.addEventListener("click", () => {
+
+    const aNumber = Number(window.prompt("Pick a new grid size: ", ""));
+
+    if (1 <= aNumber && aNumber <= 100) {
+    container.removeChild(grid);    
+    let input = aNumber;
+    createGame(input);
+    }
+    else {
+        alert("Please pick a number between 1 and 100.");
+    }
+
+})
