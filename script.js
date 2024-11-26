e528a7ecde7682689ec72efcd5f92603ba58b0af
MohanHS95 <mohanhsharan@gmail.com>
2024-11-26 12:02:09 -0500

    Allow for grid editing and resetting
    Function to add color to the grid was created upon mouseover. A button
    was added to change the size of the grid in the reserved space as well
    with a click event listener.
    
    To be able to randomize the RGB values a "getRGB" function was created
    and controlled with an if-else statement to maintain an initialized
    color.

--- script.js
+++ script.js
@@ -1,54 +1,69 @@
 const container = document.querySelector("#container");
 
 const resetButton = document.createElement("button");
-resetButton.setAttribute("id","reset");
+resetButton.setAttribute("id", "reset");
 resetButton.setAttribute("style", "border: solid 1px; height: 40px; width: 80px");
 resetButton.textContent = "Click to Reset";
 container.appendChild(resetButton);
 
+function getRGB() {
+    let rgb = 'rgb(';
+    for (let i = 0; i < 3; i++) {
+        rgb += ((Math.floor(Math.random() * 256)) + ",");
+    }
+    rgb = rgb.slice(0, -1) + ')';
+    return rgb;
+}
 function createGame(input) {
-const grid = document.createElement("div");
-grid.setAttribute("id", "grid");
-grid.setAttribute("style", "border: solid 0px; height: 600px; width: 600px");
-container.appendChild(grid);
+    const grid = document.createElement("div");
+    grid.setAttribute("id", "grid");
+    grid.setAttribute("style", "border: solid 0px; height: 600px; width: 600px");
+    container.appendChild(grid);
 
 
-const size = (600) / (input);
-for (let i = 0; i < input; i++) {
+    const size = (600) / (input);
+    for (let i = 0; i < input; i++) {
         const squarerow = document.createElement('div');
         squarerow.classList.add('squarerow');
-    for (let j = 0; j < input; j++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        square.style.width = size + 'px';
-        square.style.height = size + 'px';
-        squarerow.appendChild(square);
+        for (let j = 0; j < input; j++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.style.width = size + 'px';
+            square.style.height = size + 'px';
+            squarerow.appendChild(square);
+        }
+        grid.appendChild(squarerow);
     }
-    grid.appendChild(squarerow);
-}
 
-const squares = document.querySelectorAll(".square");
+    const squares = document.querySelectorAll(".square");
 
-squares.forEach((square) => {
+    squares.forEach((square) => {
         square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = "green";
+            let rgbColor = getRGB();
+            if (!square.style.backgroundColor) {
+                square.style.backgroundColor = rgbColor;
+            }
+        });
     });
-});
+
 }
 let input = 64;
 createGame(input);
 
-resetButton.addEventListener("click", () => {
+resetButton.addEventListener("click", resetGame);
 
+function resetGame() {
     const aNumber = Number(window.prompt("Pick a new grid size: ", ""));
 
     if (1 <= aNumber && aNumber <= 100) {
-    container.removeChild(grid);    
-    let input = aNumber;
-    createGame(input);
-    }
-    else {
+        const grid = document.querySelector("#grid");
+        if (grid) {
+            container.removeChild(grid);
+            let input = aNumber;
+            createGame(input);
+        }
+    } else {
         alert("Please pick a number between 1 and 100.");
     }
+}
 
-})
